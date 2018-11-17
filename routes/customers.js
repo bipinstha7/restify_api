71c50d7a0b3cb05e1421842fa8df052f406322dd
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-17 23:33:39 +0545

    crud application for customers

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,78 @@
 const errors = require('restify-errors')
+const Customer = require('../models/Customer')
 
 module.exports = server => {
-    server.get('/customers', (req, res, next) => {
-        res.send({msg: 'test'})
+    // Get Customers
+    server.get('/customers', async (req, res, next) => {
+        try {
+            const customers = await Customer.find({})
+            res.send(customers)
+            next()
+        } catch(err) {
+            return next(new errors.InvalidContentError(err))
+        }
+    })
+
+    // Add Customer
+    server.post('/customers', async (req, res, next) => {
+        // Check for JSON
+        if (!req.is('application/json')) {
+            return next(new errors.InvalidContentError("Expects 'application/json'"))
+        }
+
+        const { name, email, balance } = req.body
+
+        // const customer = new Customer({
+        //     name,
+        //     email,
+        //     balance
+        // })
+
+        try {
+        // const newCustomer = await customer.save()
+        await Customer.create(req.body)
+        res.send(201)
         next()
+        } catch(err) {
+            return next(new errors.InternalError(err.message))
+        }
+    })
+
+    // Get Single Customers
+    server.get('/customers/:id', async (req, res, next) => {
+        try {
+            const customer = await Customer.findById(req.params.id)
+            res.send(customer)
+            next()
+        } catch(err) {
+            return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
+        }
+    })
+
+    // Update Customer
+    server.put('/customers/:id', async (req, res, next) => {
+        // Check for JSON
+        if (!req.is('application/json')) {
+            return next(new errors.InvalidContentError("Expects 'application/json'"))
+        }
+
+        try {
+        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body)
+        res.send(200)
+        next()
+        } catch(err) {
+            return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
+        }
+    })
+
+    // Delete Customer
+    server.del('/customers/:id', async (req, res, next) => {
+        try {
+        const customer = await Customer.findByIdAndDelete(req.params.id)
+        res.send(204)
+        next()
+        } catch(err) {
+            return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
+        }
     })
 }
