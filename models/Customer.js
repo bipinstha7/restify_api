b2d790b8fcaf8a4dc55cc1a9558955fab93aa9e3
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-17 23:33:56 +0545

    customer mongoose schema

---
+++ models/Customer.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const timeStamp = require('mongoose-timestamp')
+
+const CustomerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    balance: {
+        type: Number,
+        default: 0
+    }
+})
+
+CustomerSchema.plugin(timeStamp)
+
+const Customer = mongoose.model('Customer', CustomerSchema)
+
+module.exports = Customer
