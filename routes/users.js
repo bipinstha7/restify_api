7e86251208baf979e57a05d1e7bffc54c2879669
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-18 11:15:21 +0545

    generate jwt when user logged in

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const errors = require('restify-errors')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const User  = require('../models/User')
+const auth = require('../auth')
+const config = require('../config')
 
 module.exports = server => {
     // Register User
@@ -24,5 +28,30 @@ module.exports = server => {
                 }
             })
         })
+    })
+
+    // Auth User
+    server.post('/auth', async (req, res, next) => {
+        const { email, password } = req.body
+
+        try {
+            // Authenticate User
+            const user = await auth.authenticate(email, password)
+
+            // Create JWT
+            const token = jwt.sign(user.toJSON(), config.JWT_SECRET, {
+                expiresIn: '15m'
+            })
+
+            const { iat, exp } = jwt.decode(token)
+
+            // Respond with token
+            res.send({ iat, exp, token})
+
+            next()
+        } catch (err) {
+            // User unauthorized
+            return next(new errors.UnauthorizedError(err))
+        }
     })
 }
