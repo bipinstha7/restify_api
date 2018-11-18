b94db8b1b3b44e9366b82442767b2f4223de2cac
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-18 11:14:17 +0545

    jwt protect route

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const restify = require('restify')
 const mongoose = require('mongoose')
 const config = require('./config')
+// const rjwt = require('restify-jwt-community')
 
 const server = restify.createServer()
 
 // Middleware
 server.use(restify.plugins.bodyParser())
+
+// Protect Routes
+// Protect all routes other than '/auth' 
+// server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/auth']}))
+// or it can be done in to only those routes which we want to protect as using it in the respective routes
 
 server.listen(config.PORT, () => {
     mongoose.connect(config.MONGODB_URI, {useNewUrlParser: true})
