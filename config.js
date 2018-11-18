868300759bfd69162ef1028dcfa47320aca34be4
bipinstha7 <bipin.shrestha901@gmail.com>
2018-11-18 11:14:04 +0545

    add jwt secret env

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
     ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 3000,
     URL: process.env.BASE_URL || 'http://localhost:3000',
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://restify123:restify123@ds033757.mlab.com:33757/restify_api'
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://restify123:restify123@ds033757.mlab.com:33757/restify_api',
+    JWT_SECRET: process.env.JWT_SECRET || 'secret321'
 }
