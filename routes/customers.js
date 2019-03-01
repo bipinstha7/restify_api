22ab2f538ad9c101180fe6e846e9e8486ff9b2fe
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-01 21:44:27 +0545

    create a new customer

--- routes/customers.js
+++ routes/customers.js
@@ -5,77 +5,77 @@ const config = require('../config')
 const Customer = require('../models/Customer')
 
 module.exports = server => {
-    // Get Customers
-    server.get('/customers', async (req, res, next) => {
-        try {
-            const customers = await Customer.find({})
-            res.send(customers)
-            next()
-        } catch(err) {
-            return next(new errors.InvalidContentError(err))
-        }
-    })
+	// Get Customers
+	server.get('/customers', async (req, res, next) => {
+		try {
+			const customers = await Customer.find({})
+			res.send(customers)
+			next()
+		} catch (err) {
+			return next(new errors.InvalidContentError(err))
+		}
+	})
 
-    // Add Customer
-    server.post('/customers', rjwt({ secret: config.JWT_SECRET}), async (req, res, next) => {
-        // Check for JSON
-        if (!req.is('application/json')) {
-            return next(new errors.InvalidContentError("Expects 'application/json'"))
-        }
+	// Add Customer
+	server.post('/customers', rjwt({ secret: config.JWT_SECRET }), async (req, res, next) => {
+		// Check for JSON
+		if (!req.is('application/json')) {
+			return next(new errors.InvalidContentError("Expects 'application/json'"))
+		}
 
-        const { name, email, balance } = req.body
+		const { name, email, balance } = req.body
 
-        // const customer = new Customer({
-        //     name,
-        //     email,
-        //     balance
-        // })
+		// const customer = new Customer({
+		//     name,
+		//     email,
+		//     balance
+		// })
 
-        try {
-        // const newCustomer = await customer.save()
-        await Customer.create(req.body)
-        res.send(201)
-        next()
-        } catch(err) {
-            return next(new errors.InternalError(err.message))
-        }
-    })
+		try {
+			// const newCustomer = await customer.save()
+			await Customer.create(req.body)
+			res.send(201)
+			next()
+		} catch (err) {
+			return next(new errors.InternalError(err.message))
+		}
+	})
 
-    // Get Single Customers
-    server.get('/customers/:id', async (req, res, next) => {
-        try {
-            const customer = await Customer.findById(req.params.id)
-            res.send(customer)
-            next()
-        } catch(err) {
-            return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
-        }
-    })
+	// Get Single Customers
+	server.get('/customers/:id', async (req, res, next) => {
+		try {
+			const customer = await Customer.findById(req.params.id)
+			res.send(customer)
+			next()
+		} catch (err) {
+			return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
+		}
+	})
 
-    // Update Customer
-    server.put('/customers/:id', rjwt({ secret: config.JWT_SECRET}), async (req, res, next) => {
-        // Check for JSON
-        if (!req.is('application/json')) {
-            return next(new errors.InvalidContentError("Expects 'application/json'"))
-        }
+	// Update Customer
+	server.put('/customers/:id', rjwt({ secret: config.JWT_SECRET }), async (req, res, next) => {
+		// Check for JSON
+		if (!req.is('application/json')) {
+			return next(new errors.InvalidContentError("Expects 'application/json'"))
+		}
 
-        try {
-        const customer = await Customer.findByIdAndUpdate(req.params.id, req.body)
-        res.send(200)
-        next()
-        } catch(err) {
-            return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
-        }
-    })
+		try {
+			const customer = await Customer.findByIdAndUpdate(req.params.id, req.body)
+			res.send(200)
+			next()
+		} catch (err) {
+			return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
+		}
+	})
 
-    // Delete Customer
-    server.del('/customers/:id', rjwt({ secret: config.JWT_SECRET}), async (req, res, next) => {
-        try {
-        const customer = await Customer.findByIdAndDelete(req.params.id)
-        res.send(204)
-        next()
-        } catch(err) {
-            return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
-        }
-    })
+	// Delete Customer
+	server.del('/customers/:id', rjwt({ secret: config.JWT_SECRET }), async (req, res, next) => {
+		try {
+			const customer = await Customer.findByIdAndDelete(req.params.id)
+			res.send(204)
+			next()
+		} catch (err) {
+			return next(new errors.ResourceNotFoundError(`There is no customer with id ${req.params.id}`))
+		}
+	})
 }
